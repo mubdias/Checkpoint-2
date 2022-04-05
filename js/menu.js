296edb90e2a984a66fd67ca630c5674c61e83b8e
mubdias <murilo_barberini@hotmail.com>
2022-04-05 15:17:11 -0300

    Alterações header, index, modal, responsividade

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    var botao = $('.menu');
-    var dropDown = $('.menu_lista');    
-   
-       botao.on('click', function(event){
-           dropDown.stop(true,true).slideToggle();
-           event.stopPropagation();
-       });
-   });
